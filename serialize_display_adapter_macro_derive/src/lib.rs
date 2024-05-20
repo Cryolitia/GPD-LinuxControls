bc39bdcbbe2029b04d5fb57801ae8540469397f7
北雁 Cryolitia <Cryolitia@gmail.com>
2024-05-20 22:11:08 +0800

    serialize_display_adapter_macro_derive: update

--- serialize_display_adapter_macro_derive/src/lib.rs
+++ serialize_display_adapter_macro_derive/src/lib.rs
@@ -8,7 +8,7 @@ pub fn serialize_display_adapter_macro_derive(input: TokenStream) -> TokenStream
     let ast: DeriveInput = syn::parse(input).unwrap();
     let name = &ast.ident;
     let gen = quote! {
-        impl std::fmt::Display for #name {
+        impl std::fmt::Display for #name where #name : serde::Serialize {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 write!(f, "{}", serde_json::to_string_pretty(self).unwrap_or_else(|e| format!("{}", e)))
             }
