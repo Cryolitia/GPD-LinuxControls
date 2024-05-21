bd48b5b1f9989e257941c29e88fa7ed017dec06b
北雁 Cryolitia <Cryolitia@gmail.com>
2024-05-21 16:45:43 +0800

    serialize_display_adapter_macro_derive: fix for generics

---
+++ serialize_display_adapter_macro_derive/README.md
@@ -0,0 +1,27 @@
+# Serialize Display Adapter Macro Derive
+
+```rust
+use serde::Serialize;
+use serialize_display_adapter_macro_derive::SerializeDisplayAdapter;
+
+#[derive(Serialize, SerializeDisplayAdapter)]
+struct Demo<'a> {
+    name: &'a str,
+    age: u8,
+}
+fn main() {
+    let name = "root";
+    let demo = Demo {
+        name: name.as_ref(),
+        age: 42,
+    };
+    print!("{}", demo)
+}
+
+// Should print:
+//
+// {
+//     "name": "root",
+//     "age": 42
+// }
+```
--- serialize_display_adapter_macro_derive/src/lib.rs
+++ serialize_display_adapter_macro_derive/src/lib.rs
@@ -7,8 +7,9 @@ use syn::DeriveInput;
 pub fn serialize_display_adapter_macro_derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).unwrap();
     let name = &ast.ident;
+    let (impl_generics, type_generics, where_clause) = ast.generics.split_for_impl();
     let gen = quote! {
-        impl std::fmt::Display for #name where #name : serde::Serialize {
+        impl #impl_generics std::fmt::Display for #name #type_generics #where_clause where #name #type_generics : serde::Serialize {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 write!(f, "{}", serde_json::to_string_pretty(self).unwrap_or_else(|e| format!("{}", e)))
             }
