ac68946f2546853052b22c75d20a86df380610d0
北雁 Cryolitia <Cryolitia@gmail.com>
2024-05-23 22:12:45 +0800

    serialize_display_adapter_macro_derive: document
    Signed-off-by: 北雁 Cryolitia <Cryolitia@gmail.com>

--- serialize_display_adapter_macro_derive/src/lib.rs
+++ serialize_display_adapter_macro_derive/src/lib.rs
@@ -1,5 +1,31 @@
-use proc_macro::TokenStream;
+//! ```rust
+//! use serde::Serialize;
+//! use serialize_display_adapter_macro_derive::SerializeDisplayAdapter;
+//!
+//! #[derive(Serialize, SerializeDisplayAdapter)]
+//! struct Demo<'a> {
+//!     name: &'a str,
+//!     age: u8,
+//! }
+//! fn main() {
+//!     let name = "root";
+//!     let demo = Demo {
+//!         name: name.as_ref(),
+//!         age: 42,
+//!     };
+//!     print!("{}", demo)
+//! }
+//!```
+//!
+//! Should print:
+//! ```json
+//! {
+//!     "name": "root",
+//!     "age": 42
+//! }
+//! ```
 
+use proc_macro::TokenStream;
 use quote::quote;
 use syn::DeriveInput;
 
